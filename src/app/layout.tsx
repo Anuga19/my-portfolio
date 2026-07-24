58fef97ad258e6a6cc0f0482c78bea0aae7c89ed
anuga <anuga@anugas-MacBook-Air.local>
2026-07-24 16:27:18 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import ClickSound from "@/components/ClickSound";
+import { Analytics } from "@vercel/analytics/next";
 
 const mondwest = localFont({
   src: "../../public/fonts/ppmondwest-regular.otf",
@@ -29,7 +30,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={mondwest.variable}>
-      <body className={datatype.variable}><ClickSound />{children}</body>
+      <body className={datatype.variable}><ClickSound />{children}<Analytics /></body>
     </html>
   );
 }
